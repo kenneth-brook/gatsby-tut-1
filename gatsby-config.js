e832d9a7a2d48c09e9facd31c23a4dbd3d6503f5
Kenneth Brook <kenneth.brook@hotmail.com>
2021-11-26 17:48:11 -0500

    end part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby tut 1",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
